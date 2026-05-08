35d8bc71ef2af5d83e26cf1ef0b6f315ca165337
Fede Cardoso <hernan.f.cardoso@gmail.com>
2026-05-08 12:03:39 -0300

    change version

--- wp-admin-posts-extended.php
+++ wp-admin-posts-extended.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/hcardoso81/wp-admin-posts-extended
  * Author: Hernan Cardoso
  * Author URI: https://www.linkedin.com/in/cardosohernan/
- * Version: 1.2.0
+ * Version: 2.2.0
  */
 
 defined('ABSPATH') || exit;
@@ -15,7 +15,7 @@ if (!defined('WPM_PLUGIN_PATH')) {
 }
 
 if (!defined('WPM_PLUGIN_VERSION')) {
-    define('WPM_PLUGIN_VERSION', '1.2.0');
+    define('WPM_PLUGIN_VERSION', '2.2.0');
 }
 
 require_once __DIR__ . '/vendor/autoload.php';
